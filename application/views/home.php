dab282fa5aca30ee9019427a55afe6b45aefe09e
Larry Tang <hankydream@gmail.com>
2015-06-21 21:52:59 -0400

    background changes

---
+++ application/views/home.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link rel="stylesheet" type="text/css" href="../../assets/css/reset.css">
+	<link rel="stylesheet" type="text/css" href="../../assets/css/home.css">
+</head>
+<body>
+
+	<div id="header">
+		<div id="background_container">
+			<img src="../../assets/images/bk1.jpg" style="height:100%; width:100%">
+		</div>
+
+	</div>
+
+
+</body>
+</html>
